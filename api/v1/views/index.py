64ff9592733e2efa3162e2547353cdd427a589cc
prosperemebo <emeboprosper@gmail.com>
2023-10-29 02:26:13 +0100

    Task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,3 +13,27 @@ def status():
     response = make_response(json.dumps(res), 200)
     response.headers['Content-Type'] = 'application/json'
     return response
+
+
+@app_views.route("/stats")
+def stats():
+    """Handle API stats."""
+    from models import storage
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
+    res = {
+        "amenities": storage.count(Amenity),
+        "cities":  storage.count(City),
+        "places":  storage.count(Place),
+        "reviews":  storage.count(Review),
+        "states":  storage.count(State),
+        "users":  storage.count(User)
+    }
+    response = make_response(json.dumps(res), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
