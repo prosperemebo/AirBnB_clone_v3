ddd94cdb559c622aab45491f65b83f918d191a9a
prosperemebo <emeboprosper@gmail.com>
2023-11-16 20:16:59 +0100

    New Push

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,14 @@ from flask import Flask, make_response
 from flask_cors import CORS
 from api.v1.views import app_views
 from os import getenv
+from dotenv import load_dotenv
 import json
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+
+load_dotenv()
 
 host = getenv("HBNB_API_HOST")
 port = getenv("HBNB_API_PORT")
